776b3493b184ada3ddee6898fc02f38c88652c55
LunaTomas <Conradoluna20@gmail.com>
2025-09-14 19:55:25 -0300

    Agregar archivos de proyecto.

---
+++ pyrLunaReservaDeCabañas/frmPrincipal.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace pyrLunaReservaDeCabañas
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            grpTipoDeCabaña = new GroupBox();
+            mskDias = new MaskedTextBox();
+            cmbPersonas = new ComboBox();
+            cmbTipo = new ComboBox();
+            lblDias = new Label();
+            lblPersonas = new Label();
+            lblTipo = new Label();
+            grpAdicionales = new GroupBox();
+            chkAdicionales = new CheckedListBox();
+            grpFormaDePago = new GroupBox();
+            cmbTarjetas = new ComboBox();
+            lblTarjetas = new Label();
+            rdbTarjeta = new RadioButton();
+            rdbEfectivo = new RadioButton();
+            grpTitularDeLaReserva = new GroupBox();
+            txtTelefonos = new TextBox();
+            txtNombre = new TextBox();
+            lblTelefono = new Label();
+            lblNombre = new Label();
+            btnAceptar = new Button();
+            grpTipoDeCabaña.SuspendLayout();
+            grpAdicionales.SuspendLayout();
+            grpFormaDePago.SuspendLayout();
+            grpTitularDeLaReserva.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grpTipoDeCabaña
+            // 
+            grpTipoDeCabaña.Controls.Add(mskDias);
+            grpTipoDeCabaña.Controls.Add(cmbPersonas);
+            grpTipoDeCabaña.Controls.Add(cmbTipo);
+            grpTipoDeCabaña.Controls.Add(lblDias);
+            grpTipoDeCabaña.Controls.Add(lblPersonas);
+            grpTipoDeCabaña.Controls.Add(lblTipo);
+            grpTipoDeCabaña.Font = new Font("Segoe UI", 14.25F);
+            grpTipoDeCabaña.Location = new Point(18, 15);
+            grpTipoDeCabaña.Name = "grpTipoDeCabaña";
+            grpTipoDeCabaña.Size = new Size(599, 100);
+            grpTipoDeCabaña.TabIndex = 0;
+            grpTipoDeCabaña.TabStop = false;
+            grpTipoDeCabaña.Text = "Tipo de Cabaña";
+            // 
+            // mskDias
+            // 
+            mskDias.Location = new Point(540, 28);
+            mskDias.Mask = "99999";
+            mskDias.Name = "mskDias";
+            mskDias.PromptChar = ' ';
+            mskDias.Size = new Size(53, 33);
+            mskDias.TabIndex = 2;
+            mskDias.ValidatingType = typeof(int);
+            mskDias.TextChanged += mskDias_TextChanged;
+            // 
+            // cmbPersonas
+            // 
+            cmbPersonas.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbPersonas.FormattingEnabled = true;
+            cmbPersonas.Location = new Point(359, 28);
+            cmbPersonas.Name = "cmbPersonas";
+            cmbPersonas.Size = new Size(121, 33);
+            cmbPersonas.TabIndex = 1;
+            // 
+            // cmbTipo
+            // 
+            cmbTipo.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbTipo.FormattingEnabled = true;
+            cmbTipo.Location = new Point(65, 28);
+            cmbTipo.Name = "cmbTipo";
+            cmbTipo.Size = new Size(195, 33);
+            cmbTipo.TabIndex = 0;
+            cmbTipo.SelectedIndexChanged += cmbTipo_SelectedIndexChanged;
+            // 
+            // lblDias
+            // 
+            lblDias.AutoSize = true;
+            lblDias.Location = new Point(486, 31);
+            lblDias.Name = "lblDias";
+            lblDias.Size = new Size(48, 25);
+            lblDias.TabIndex = 5;
+            lblDias.Text = "Días";
+            // 
+            // lblPersonas
+            // 
+            lblPersonas.AutoSize = true;
+            lblPersonas.Location = new Point(266, 31);
+            lblPersonas.Name = "lblPersonas";
+            lblPersonas.Size = new Size(87, 25);
+            lblPersonas.TabIndex = 4;
+            lblPersonas.Text = "Personas";
+            // 
+            // lblTipo
+            // 
+            lblTipo.AutoSize = true;
+            lblTipo.Location = new Point(6, 31);
+            lblTipo.Name = "lblTipo";
+            lblTipo.Size = new Size(49, 25);
+            lblTipo.TabIndex = 3;
+            lblTipo.Text = "Tipo";
+            // 
+            // grpAdicionales
+            // 
+            grpAdicionales.Controls.Add(chkAdicionales);
+            grpAdicionales.Font = new Font("Segoe UI", 14.25F);
+            grpAdicionales.Location = new Point(18, 121);
+            grpAdicionales.Name = "grpAdicionales";
+            grpAdicionales.Size = new Size(301, 139);
+            grpAdicionales.TabIndex = 1;
+            grpAdicionales.TabStop = false;
+            grpAdicionales.Text = "Adicionales";
+            // 
+            // chkAdicionales
+            // 
+            chkAdicionales.FormattingEnabled = true;
+            chkAdicionales.Items.AddRange(new object[] { "Cocina", "Heladera", "Televisor" });
+            chkAdicionales.Location = new Point(6, 34);
+            chkAdicionales.Name = "chkAdicionales";
+            chkAdicionales.Size = new Size(120, 88);
+            chkAdicionales.TabIndex = 0;
+            // 
+            // grpFormaDePago
+            // 
+            grpFormaDePago.Controls.Add(cmbTarjetas);
+            grpFormaDePago.Controls.Add(lblTarjetas);
+            grpFormaDePago.Controls.Add(rdbTarjeta);
+            grpFormaDePago.Controls.Add(rdbEfectivo);
+            grpFormaDePago.Font = new Font("Segoe UI", 14.25F);
+            grpFormaDePago.Location = new Point(326, 121);
+            grpFormaDePago.Name = "grpFormaDePago";
+            grpFormaDePago.Size = new Size(291, 139);
+            grpFormaDePago.TabIndex = 2;
+            grpFormaDePago.TabStop = false;
+            grpFormaDePago.Text = "Forma de Pago";
+            // 
+            // cmbTarjetas
+            // 
+            cmbTarjetas.Font = new Font("Segoe UI", 15.75F);
+            cmbTarjetas.FormattingEnabled = true;
+            cmbTarjetas.Location = new Point(112, 95);
+            cmbTarjetas.Name = "cmbTarjetas";
+            cmbTarjetas.Size = new Size(173, 38);
+            cmbTarjetas.TabIndex = 2;
+            // 
+            // lblTarjetas
+            // 
+            lblTarjetas.AutoSize = true;
+            lblTarjetas.Location = new Point(112, 67);
+            lblTarjetas.Name = "lblTarjetas";
+            lblTarjetas.Size = new Size(76, 25);
+            lblTarjetas.TabIndex = 3;
+            lblTarjetas.Text = "Tarjetas";
+            // 
+            // rdbTarjeta
+            // 
+            rdbTarjeta.AutoSize = true;
+            rdbTarjeta.Location = new Point(6, 63);
+            rdbTarjeta.Name = "rdbTarjeta";
+            rdbTarjeta.Size = new Size(86, 29);
+            rdbTarjeta.TabIndex = 1;
+            rdbTarjeta.TabStop = true;
+            rdbTarjeta.Text = "Tarjeta";
+            rdbTarjeta.UseVisualStyleBackColor = true;
+            rdbTarjeta.CheckedChanged += rdbTarjeta_CheckedChanged;
+            // 
+            // rdbEfectivo
+            // 
+            rdbEfectivo.AutoSize = true;
+            rdbEfectivo.Location = new Point(6, 32);
+            rdbEfectivo.Name = "rdbEfectivo";
+            rdbEfectivo.Size = new Size(96, 29);
+            rdbEfectivo.TabIndex = 0;
+            rdbEfectivo.TabStop = true;
+            rdbEfectivo.Text = "Efectivo";
+            rdbEfectivo.UseVisualStyleBackColor = true;
+            // 
+            // grpTitularDeLaReserva
+            // 
+            grpTitularDeLaReserva.Controls.Add(txtTelefonos);
+            grpTitularDeLaReserva.Controls.Add(txtNombre);
+            grpTitularDeLaReserva.Controls.Add(lblTelefono);
+            grpTitularDeLaReserva.Controls.Add(lblNombre);
+            grpTitularDeLaReserva.Font = new Font("Segoe UI", 14.25F);
+            grpTitularDeLaReserva.Location = new Point(18, 266);
+            grpTitularDeLaReserva.Name = "grpTitularDeLaReserva";
+            grpTitularDeLaReserva.Size = new Size(599, 108);
+            grpTitularDeLaReserva.TabIndex = 3;
+            grpTitularDeLaReserva.TabStop = false;
+            grpTitularDeLaReserva.Text = "Titular de la Reserva";
+            // 
+            // txtTelefonos
+            // 
+            txtTelefonos.Location = new Point(104, 65);
+            txtTelefonos.Name = "txtTelefonos";
+            txtTelefonos.Size = new Size(477, 33);
+            txtTelefonos.TabIndex = 1;
+            txtTelefonos.TextChanged += txtTelefonos_TextChanged;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(104, 26);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(477, 33);
+            txtNombre.TabIndex = 0;
+            txtNombre.TextChanged += txtNombre_TextChanged;
+            // 
+            // lblTelefono
+            // 
+            lblTelefono.AutoSize = true;
+            lblTelefono.Location = new Point(6, 68);
+            lblTelefono.Name = "lblTelefono";
+            lblTelefono.Size = new Size(84, 25);
+            lblTelefono.TabIndex = 3;
+            lblTelefono.Text = "Teléfono";
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(6, 29);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(81, 25);
+            lblNombre.TabIndex = 2;
+            lblNombre.Text = "Nombre";
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Font = new Font("Segoe UI", 14.25F);
+            btnAceptar.Location = new Point(496, 380);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(121, 38);
+            btnAceptar.TabIndex = 4;
+            btnAceptar.Text = "Aceptar";
+            btnAceptar.UseVisualStyleBackColor = true;
+            btnAceptar.Click += btnAceptar_Click;
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(635, 433);
+            Controls.Add(btnAceptar);
+            Controls.Add(grpTitularDeLaReserva);
+            Controls.Add(grpFormaDePago);
+            Controls.Add(grpAdicionales);
+            Controls.Add(grpTipoDeCabaña);
+            Name = "frmPrincipal";
+            Text = "Reservas de Cabañas";
+            Load += frmPrincipal_Load;
+            grpTipoDeCabaña.ResumeLayout(false);
+            grpTipoDeCabaña.PerformLayout();
+            grpAdicionales.ResumeLayout(false);
+            grpFormaDePago.ResumeLayout(false);
+            grpFormaDePago.PerformLayout();
+            grpTitularDeLaReserva.ResumeLayout(false);
+            grpTitularDeLaReserva.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox grpTipoDeCabaña;
+        private GroupBox grpAdicionales;
+        private GroupBox grpFormaDePago;
+        private GroupBox grpTitularDeLaReserva;
+        private MaskedTextBox mskDias;
+        private ComboBox cmbPersonas;
+        private ComboBox cmbTipo;
+        private Label lblDias;
+        private Label lblPersonas;
+        private Label lblTipo;
+        private CheckedListBox chkAdicionales;
+        private ComboBox cmbTarjetas;
+        private Label lblTarjetas;
+        private RadioButton rdbTarjeta;
+        private RadioButton rdbEfectivo;
+        private TextBox txtTelefonos;
+        private TextBox txtNombre;
+        private Label lblTelefono;
+        private Label lblNombre;
+        private Button btnAceptar;
+    }
+}
---
+++ pyrLunaReservaDeCabañas/frmPrincipal.cs
@@ -0,0 +1,143 @@
+namespace pyrLunaReservaDeCabañas
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+
+        {
+            InitializeComponent();
+        }
+        private void ActualizarPersonas(string tipo)// actualiza las opciones de personas según el tipo de cabaña
+        {
+            cmbPersonas.Items.Clear(); // limpia las opciones
+
+            int max = (tipo == "A") ? 4 : 8; // si es A 4, sino 8
+
+            for (int i = 1; i <= max; i++)
+                cmbPersonas.Items.Add(i);
+
+            cmbPersonas.SelectedIndex = 0; // selecciona el primero (1)
+        }
+        private void ValidarAceptar() // habilita o deshabilita el botón Aceptar
+        {
+            int dias;
+            bool diasValidos = int.TryParse(mskDias.Text, out dias) && dias > 0;
+
+            bool nombreValido = !string.IsNullOrWhiteSpace(txtNombre.Text);
+            bool telValido = !string.IsNullOrWhiteSpace(txtTelefonos.Text);
+
+            btnAceptar.Enabled = diasValidos && nombreValido && telValido;
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            {
+                mskDias.Focus();
+                // Tipo de cabaña por defecto
+                cmbTipo.Items.AddRange(new string[] { "Tipo A (4 max)", "Tipo B (8 max)" });
+                cmbTipo.SelectedIndex = 0;
+                ActualizarPersonas("A");
+
+                // Personas por defecto (tipo A)
+                cmbPersonas.SelectedIndex = 0;
+
+                // Días inicial
+                mskDias.Text = "1";
+
+                // Adicionales vacíos
+                chkAdicionales.Items.AddRange(new string[] { "Cocina", "Heladera", "Televisión" });
+                for (int i = 0; i < chkAdicionales.Items.Count; i++)
+                    chkAdicionales.SetItemChecked(i, false);
+
+                // Forma de pago
+                rdbEfectivo.Checked = true;
+                rdbTarjeta.Checked = false;
+
+                cmbTarjetas.Items.AddRange(new string[] { "Card Red", "Card Green", "Card Blue" });
+                cmbTarjetas.Enabled = false;
+                cmbTarjetas.SelectedIndex = -1;
+
+                // Datos del titular
+                txtNombre.Text = "";
+                txtTelefonos.Text = "";
+
+                // Botón aceptar deshabilitado
+                btnAceptar.Enabled = false;
+            }
+        }
+
+        private void cmbTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string tipo = cmbTipo.SelectedItem.ToString();
+            ActualizarPersonas(tipo);
+        }
+
+        private void rdbTarjeta_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbTarjeta.Checked)
+            {
+                cmbTarjetas.Enabled = true;
+                cmbTarjetas.SelectedIndex = 0; // Card Red por defecto
+            }
+            else
+            {
+                cmbTarjetas.Enabled = false;
+                cmbTarjetas.SelectedIndex = -1;
+            }
+        }
+
+        private void mskDias_TextChanged(object sender, EventArgs e)
+        {
+            ValidarAceptar();
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            ValidarAceptar();
+        }
+
+        private void txtTelefonos_TextChanged(object sender, EventArgs e)
+        {
+            ValidarAceptar();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            //Obtener datos de la reserva
+            string tipo = cmbTipo.SelectedItem.ToString();
+            int personas = int.Parse(cmbPersonas.SelectedItem.ToString());
+            int dias = int.Parse(mskDias.Text);
+
+            //Calcular precio base + personas
+            double precioBase = (tipo == "Tipo A (4 max)") ? 20 : 34;
+            double costoDiario = precioBase + personas; // +1 por persona
+
+            //Sumar adicionales
+            foreach (var item in chkAdicionales.CheckedItems)
+            {
+                if (item.ToString() == "Cocina") costoDiario += 1;
+                if (item.ToString() == "Heladera") costoDiario += 1.5;
+                if (item.ToString() == "Televisión") costoDiario += 2;
+            }
+
+            //Calcular total por días
+            double total = costoDiario * dias;
+
+            //Aplicar recargo si es tarjeta
+            if (rdbTarjeta.Checked && cmbTarjetas.SelectedIndex != -1)
+            {
+                string tarjeta = cmbTarjetas.SelectedItem.ToString();
+                if (tarjeta == "Card Red")
+                    total *= 1.10; // +10%
+                else
+                    total *= 1.20; // +20%
+            }
+
+            //Mostrar resultado en cuadro de diálogo
+            MessageBox.Show($"Costo total de la reserva: US$ {total:F2}", "Reserva confirmada");
+
+            //Volver al estado inicial
+            frmPrincipal_Load(sender, e);
+        }
+    }
+}
